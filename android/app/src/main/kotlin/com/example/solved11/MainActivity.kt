61bb06693ed4d7ed9c23e8be3a6b9708400e497e
sagor <sagorrrr11@gmail.com>
2023-09-25 20:17:38 +0600

    hii

---
+++ android/app/src/main/kotlin/com/example/solved11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.solved11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:livelocation/app.dart';
+
+void main() {
+  runApp(const LiveLocation());
+}
